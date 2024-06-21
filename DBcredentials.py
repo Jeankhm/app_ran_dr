d860487d8026ac6d257cc09a2432eb776cd4c2ad
Jeankhm <jkrlos.jchm@gmail.com>
2024-06-21 11:10:52 -0500

    Initial commit

---
+++ DBcredentials.py
@@ -0,0 +1,8 @@
+# BD PostgreSQLde desarrollo de Sergio
+BD_DATA_PARAMS = {
+    "host":"10.40.111.100",
+    "database":"analytics_dev",
+    "user":"reobertocuervo",
+    "password":"w0m_2024*",
+    "port":"5432"
+}
